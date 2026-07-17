d31fe53915d9efb2baef2ac443ef8ac9c5196aed
student-sambhav <sambhav11052005@gmail.com>
2026-07-17 21:39:09 +0530

    Time: 102 ms (7.22%) | Memory: 75.9 MB (63.65%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+int lcs(string &s,string &s1,int n){
+    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0||j==0){
+                dp[i][j]=0;
+            }
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(s[i-1]==s1[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][n];
+}
+    int longestPalindromeSubseq(string s) {
+        string s1=s;
+        reverse(s1.begin(),s1.end());
+        int n=s.size();
+        return lcs(s,s1,n);
+    }
+};
