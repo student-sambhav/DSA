0384bd469d3c01db399bddf6a3c62ca74d742af1
student-sambhav <sambhav11052005@gmail.com>
2026-07-18 17:11:42 +0530

    Time: 425 ms (35.25%) | Memory: 211.5 MB (39.80%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-int solve(int i,int buy,int cap,vector<int>&prices,int n,
-vector<vector<vector<int>>> &dp){
-   if(i==n || cap==0) return 0;
-   int profit=0;
-   if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
-   if(buy){
-    profit=max(-prices[i]+solve(i+1,0,cap,prices,n,dp), // buy
-    solve(i+1,1,cap,prices,n,dp)); //skip
-   }
-   else{
-    profit=max(prices[i]+solve(i+1,1,cap-1,prices,n,dp),//sell
-    solve(i+1,0,cap,prices,n,dp));//skip
-   }
-   return dp[i][buy][cap]=profit;
-}
-    int maxProfit(vector<int>& prices) {
+ int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(0,1,2,prices,n,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int i=n-1;i>=0;i--){
+            for(int cap=1;cap<=2;cap++){
+            dp[i][1][cap]=max(-prices[i]+dp[i+1][0][cap],
+            dp[i+1][1][cap]);
+
+            dp[i][0][cap]=max(prices[i]+dp[i+1][1][cap-1],
+            dp[i+1][0][cap]);
+            }
+
+        }
+        return dp[0][1][2];
     }
 };
