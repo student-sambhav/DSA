c60ed04794c6cde99448854a502f9bdcd1379bf9
student-sambhav <sambhav11052005@gmail.com>
2026-07-29 16:22:47 +0530

    Time: 312 ms (30.41%) | Memory: 137.5 MB (33.13%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n%k) return false;
+        map<int,int>mpp;
+        for(auto it:nums){
+            mpp[it]++;
+        }
+        while(!mpp.empty()){
+            int curr=mpp.begin()->first;
+            for(int i=0;i<k;i++){
+                if(mpp[curr+i]==0){
+                    return false;
+                }
+                mpp[curr+i]--;
+                if(mpp[curr+i]==0){
+                    mpp.erase(curr+i);
+                }
+            }
+        }
+        return true;
+    }
+};
