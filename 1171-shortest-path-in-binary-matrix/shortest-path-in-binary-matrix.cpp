aa19664ddaf2877038abcd26f6f592e6c68fdf5c
student-sambhav <sambhav11052005@gmail.com>
2026-07-30 14:56:32 +0530

    Time: 264 ms (5.02%) | Memory: 82.8 MB (5.01%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+vector<vector<int>>dir={{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        if(n==0 || grid[0][0]==1 ||  grid[n-1][n-1] == 1){
+            return -1;
+        }
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        grid[0][0]=1;
+        int level=0;
+        while(!q.empty()){
+            int s=q.size();
+            while(s--){
+                auto curr=q.front();
+                q.pop();
+                int x=curr.first;
+                int y=curr.second;
+
+                if(x==n-1 && y==n-1) return level+1;
+
+                for(auto it:dir){
+                    int i=x+it[0];
+                    int j=y+it[1];
+
+                    if(i>=0 && i<n && j>=0 && j<n && grid[i][j]==0){
+                        q.push({i,j});
+                        grid[i][j]=1;
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
+    }
+};
